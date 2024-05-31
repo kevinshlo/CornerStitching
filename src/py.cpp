69fe4343f67ee68db2b051088fb002c0ab09d138
kevinshlo <kevinshlo@IP-214-115.cs.nycu.edu.tw>
2024-05-31 16:14:17 +0800

    add APIs in PyTile: NeighborFinding(s), Delete

--- src/py.cpp
+++ src/py.cpp
@@ -9,7 +9,7 @@
 
 class PyTile {
  public:
-  const Stitch& s_;
+  Stitch& s_;
   Id id_;
   std::optional<Tile> At() const { return s_.At(id_); }
   std::optional<Tile> At(Id id) const { return s_.At(id); }
@@ -18,7 +18,7 @@ class PyTile {
  public:
   PyTile() = delete;
   PyTile(const PyTile& t) : s_(t.s_), id_(t.id_) {}
-  PyTile(const Stitch& s, Id id = kNullId) : s_(s), id_(id) {}
+  PyTile(Stitch& s, Id id = kNullId) : s_(s), id_(id) {}
 
   bool Exist() const { return s_.Exist(id_); }
   std::optional<Pt> Coord() const {
@@ -64,6 +64,35 @@ class PyTile {
     else
       return std::nullopt;
   }
+  std::vector<PyTile> RightNeighborFinding() const {
+    auto ids = s_.RightNeighborFinding(id_);
+    std::vector<PyTile> ret;
+    ret.reserve(ids.size());
+    for (auto id : ids) ret.push_back(PyTile(s_, id));
+    return ret;
+  }
+  std::vector<PyTile> LeftNeighborFinding() const {
+    auto ids = s_.LeftNeighborFinding(id_);
+    std::vector<PyTile> ret;
+    ret.reserve(ids.size());
+    for (auto id : ids) ret.push_back(PyTile(s_, id));
+    return ret;
+  }
+  std::vector<PyTile> TopNeighborFinding() const {
+    auto ids = s_.TopNeighborFinding(id_);
+    std::vector<PyTile> ret;
+    ret.reserve(ids.size());
+    for (auto id : ids) ret.push_back(PyTile(s_, id));
+    return ret;
+  }
+  std::vector<PyTile> BottomNeighborFinding() const {
+    auto ids = s_.BottomNeighborFinding(id_);
+    std::vector<PyTile> ret;
+    ret.reserve(ids.size());
+    for (auto id : ids) ret.push_back(PyTile(s_, id));
+    return ret;
+  }
+  int Delete() { return s_.Delete(id_).has_value() ? 0 : 1; }
 };
 
 typedef std::pair<Len, Len> Len2;
@@ -81,8 +110,7 @@ class PyStitch {
 
   size_t NumTiles() const { return s_.NumTiles(); }
 
-  OptPyTile PointFinding(const Pt& pt,
-                         const OptPyTile& start = std::nullopt) const {
+  OptPyTile PointFinding(const Pt& pt, const OptPyTile& start = std::nullopt) {
     Id id = start.has_value() ? s_.PointFinding(pt, start.value().id_)
                               : s_.PointFinding(pt);
     if (s_.Exist(id))
@@ -91,35 +119,23 @@ class PyStitch {
       return std::nullopt;
   }
   std::vector<PyTile> RightNeighborFinding(const PyTile& t) const {
-    auto ids = s_.RightNeighborFinding(t.id_);
-    std::vector<PyTile> ret;
-    ret.reserve(ids.size());
-    for (auto id : ids) ret.push_back(PyTile(s_, id));
-    return ret;
+    if (&t.s_ != &s_) return {};
+    return t.RightNeighborFinding();
   }
   std::vector<PyTile> LeftNeighborFinding(const PyTile& t) const {
-    auto ids = s_.LeftNeighborFinding(t.id_);
-    std::vector<PyTile> ret;
-    ret.reserve(ids.size());
-    for (auto id : ids) ret.push_back(PyTile(s_, id));
-    return ret;
+    if (&t.s_ != &s_) return {};
+    return t.LeftNeighborFinding();
   }
   std::vector<PyTile> TopNeighborFinding(const PyTile& t) const {
-    auto ids = s_.TopNeighborFinding(t.id_);
-    std::vector<PyTile> ret;
-    ret.reserve(ids.size());
-    for (auto id : ids) ret.push_back(PyTile(s_, id));
-    return ret;
+    if (&t.s_ != &s_) return {};
+    return t.TopNeighborFinding();
   }
   std::vector<PyTile> BottomNeighborFinding(const PyTile& t) const {
-    auto ids = s_.BottomNeighborFinding(t.id_);
-    std::vector<PyTile> ret;
-    ret.reserve(ids.size());
-    for (auto id : ids) ret.push_back(PyTile(s_, id));
-    return ret;
+    if (&t.s_ != &s_) return {};
+    return t.BottomNeighborFinding();
   }
   OptPyTile AreaSearch(const Pt& coord, const Pt& size,
-                       const OptPyTile& start = std::nullopt) const {
+                       const OptPyTile& start = std::nullopt) {
     if (!coord.InQuadrantI() || !size.IsSize()) return std::nullopt;
     Tile area(coord, size);
     Id id = start.has_value() ? s_.AreaSearch(area, start.value().id_)
@@ -130,7 +146,7 @@ class PyStitch {
       return std::nullopt;
   }
   std::vector<PyTile> AreaEnum(const Pt& coord, const Pt& size,
-                               const OptPyTile& start = std::nullopt) const {
+                               const OptPyTile& start = std::nullopt) {
     if (!coord.InQuadrantI() || !size.IsSize()) return {};
     Tile area(coord, size);
     auto ids = start.has_value() ? s_.AreaEnum(area, start.value().id_)
@@ -148,8 +164,11 @@ class PyStitch {
     else
       return std::nullopt;
   }
-  int Delete(const PyTile& dead) {
-    return s_.Delete(dead.id_).has_value() ? 0 : 1;
+  int Delete(PyTile& dead) {
+    if (&dead.s_ != &s_)
+      return 1;
+    else
+      return dead.Delete();
   }
 };
 
@@ -168,16 +187,23 @@ PYBIND11_MODULE(_stitch, m) {
           py::is_operator())
       .def(py::self * Len())
       .def(py::self == py::self);
+
   py::class_<PyTile>(m, "Tile")
       .def(py::init<const PyTile&>())
-      .def("exist", &PyTile::Exist)
-      .def("coord", &PyTile::Coord)
-      .def("size", &PyTile::Size)
-      .def("is_space", &PyTile::IsSpace)
-      .def("bl", &PyTile::BL)
-      .def("lb", &PyTile::LB)
-      .def("tr", &PyTile::TR)
-      .def("rt", &PyTile::RT);
+      .def_property_readonly("exist", &PyTile::Exist)
+      .def_property_readonly("coord", &PyTile::Coord)
+      .def_property_readonly("size", &PyTile::Size)
+      .def_property_readonly("is_space", &PyTile::IsSpace)
+      .def_property_readonly("bl", &PyTile::BL)
+      .def_property_readonly("lb", &PyTile::LB)
+      .def_property_readonly("tr", &PyTile::TR)
+      .def_property_readonly("rt", &PyTile::RT)
+      .def("right_neighbors", &PyTile::RightNeighborFinding)
+      .def("left_neighbors", &PyTile::LeftNeighborFinding)
+      .def("top_neighbors", &PyTile::TopNeighborFinding)
+      .def("bottom_neighbors", &PyTile::BottomNeighborFinding)
+      .def("delete", &PyTile::Delete);
+
   py::class_<PyStitch>(m, "Stitch")
       .def(py::init<const PyStitch&>())
       .def(py::init<const Pt&, const Pt&>())
